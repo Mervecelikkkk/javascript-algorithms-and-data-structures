cf5fdd0cec0d1702be571b920cf1684c7db5d995
mervecelikkkk <merve.celik56@outlook.com>
2023-03-25 17:35:22 +0300

    Add Find More Than the First Match

---
+++ regular-expressions/find-more-than-the-first-match/index.js
@@ -0,0 +1,6 @@
+
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/gi;
+let result = twinkleStar.match(starRegex);
+
+//And here match returns the value [ 'Twinkle', 'twinkle' ]

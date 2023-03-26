389c55cfc4cc0f4407970ddfc52adff24eaf0195
mervecelikkkk <merve.celik56@outlook.com>
2023-03-26 18:47:13 +0300

    Add Match Letters of the Alphabet

---
+++ regular-expressions/match-letters-of-the-alphabet/index.js
@@ -0,0 +1,6 @@
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; 
+let result = quoteSample.match(alphabetRegex); 
+
+//  match 35 items in quoteSample

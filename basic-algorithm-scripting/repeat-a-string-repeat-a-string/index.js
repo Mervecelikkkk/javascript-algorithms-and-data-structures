f933bca1ec473d53d6b7a8b14318ff80c5674458
mervecelikkkk <merve.celik56@outlook.com>
2023-04-20 18:02:35 +0300

    Add Repeat a String Repeat a String

---
+++ basic-algorithm-scripting/repeat-a-string-repeat-a-string/index.js
@@ -0,0 +1,7 @@
+
+function repeatStringNumTimes(str, num) {
+    if (num < 1) return '';
+    return num === 1 ? str : str + repeatStringNumTimes(str, num-1);
+  }
+  
+  repeatStringNumTimes("abc", 3);

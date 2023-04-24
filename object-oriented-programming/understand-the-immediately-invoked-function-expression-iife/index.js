fd663c3ed134073aeabff5dc9025668efbde75fa
mervecelikkkk <merve.celik56@outlook.com>
2023-04-25 01:33:35 +0300

    Add Understand the Immediately Invoked Function Expression (IIFE)

---
+++ object-oriented-programming/understand-the-immediately-invoked-function-expression-iife/index.js
@@ -0,0 +1,26 @@
+
+// Approach 1:
+
+(function () {
+    console.log("A cozy nest is ready");
+  })();
+
+// Approach 2:
+
+  (function () {
+    console.log("A cozy nest is ready");
+  }());
+
+  
+// Approach 3:
+
+  (() => {
+    console.log("A cozy nest is ready");
+  })();
+
+
+// Approach 4:
+
+  (() =>
+    console.log("A cozy nest is ready")
+  )();

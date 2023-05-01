12d816051318715334374038c992e3d16c413c68
mervecelikkkk <merve.celik56@outlook.com>
2023-05-02 01:21:58 +0300

    Add Map the Debris

---
+++ intermediate-algorithm-scripting/map-the-debris/index.js
@@ -0,0 +1,17 @@
+
+function orbitalPeriod(arr) {
+    const GM = 398600.4418;
+    const earthRadius = 6367.4447;
+    const TAU = 2 * Math.PI;
+    return arr.map(function(obj) {
+      return {
+        name: obj.name,
+        orbitalPeriod: Math.round(TAU * Math.sqrt(Math.pow(obj.avgAlt+earthRadius, 3)/GM))
+      };
+    });
+  }
+
+  // test case
+
+  
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
